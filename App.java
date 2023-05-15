38d579242a379dd8aee9a455c35c0746ce81cceb
xyj <109475385+xyj1001@users.noreply.github.com>
2023-05-15 17:27:45 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+package wificonnector;
+
+
+import wificonnector.GUI.MainJf;
+import wificonnector.Utils.Font;
+
+public class App {
+    public static void main(String[] args) {
+        Font.InitGlobalFont();
+        MainJf mainJf = new MainJf();
+    }
+}
